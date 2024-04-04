e164ab600f2440e09c52ae7a65da2e7c57891785
Lucas <lucasrisson@gmail.com>
2024-04-04 12:58:36 -0300

    Corrected line 2 and added ports

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 Create and send files from Cisco 200 configuration to TFTP Server via FTP
 
 Variables to be changed:
-FTP_SERVER
-FTP_USER
-FTP_PASS
+FTP_SERVER,
+FTP_USER,
+FTP_PASS,
+FTP_PORT (if needed)
--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def replace_words(text, dic):
 FTP_SERVER = 'SERVER'
 FTP_USER = 'USER'
 FTP_PASS = 'PASS'
+FTP_PORT = 21
 
 SIP_SERVER = input("Insira o servidor: ")
 SIP_USER = input("Insira o usuário: ")
@@ -26,8 +27,7 @@ while LINE != 1 and LINE != 2:
     LINE = int(input("Você deseja configurar line 1 ou 2: "))
 
 if LINE == 2:
-    ATA_MAC = ATA_MAC[0:-2] + '01'
-
+    ATA_MAC = ATA_MAC[2:] + '01'
 FILE_NAME = 'ATA' + ATA_MAC + '.cnf.xml'
 
 with open('ATA', 'r') as file:
@@ -44,7 +44,10 @@ filedata = replace_words(filedata, replace)
 with open(FILE_NAME, 'w') as file:
     file.write(filedata)
 
-session = FTP(FTP_SERVER, FTP_USER, FTP_PASS)
+session = FTP()
+session.connect(FTP_SERVER, FTP_PORT)
+session.login(FTP_USER, FTP_PASS)
+
 file = open(FILE_NAME, 'rb')
 session.storbinary(f'STOR {FILE_NAME}', file)
 file.close()
